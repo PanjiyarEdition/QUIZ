e12d88a384c6ff245509ee7ba73136b326cc6e21
PanjiyarEdition <jigarpanjiyar@gmail.com>
2024-01-01 23:51:48 +0530

    main

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function ($) {
+  //open-close submenu on mobile
+  $(".cd-main-nav").on("click", function (event) {
+    if ($(event.target).is(".cd-main-nav"))
+      $(this).children("ul").toggleClass("is-visible");
+  });
+});
